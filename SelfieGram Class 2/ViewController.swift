02d7291912104e7f4d4f314ffaeb38ba0974b19c
jesse Elliott <elliott.jesse@gmail.com>
2016-01-07 18:50:20 -0800

    Initial Commit

---
+++ SelfieGram Class 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelfieGram Class 2
+//
+//  Created by Jesse on 2016-01-07.
+//  Copyright © 2016 Jesse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
